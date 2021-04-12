ecaae2db54c0f998e380a3d5d6e123de72dd5d6f
Jordi Carlos <66413638+mutRec@users.noreply.github.com>
2021-04-12 12:41:45 +0200

    Update app.js

--- app.js
+++ app.js
@@ -4,11 +4,12 @@ const datastore = require('nedb'); // La mini-BBDD Documental (nedb).
 
 // Creo l'aplicació (web) utilitzant el framework express.
 const app = express();
-const port = 80;
+//const port = 80;
 
+app.set('port', 80);
 // Creo servidor Http que escoltarà peticions contra el port
 // que hem definit.
-app.listen(port, () => {
+app.listen(app.get('port'), () => {
     console.log(`Servidor escoltant en el port ${port}`)
 });
 

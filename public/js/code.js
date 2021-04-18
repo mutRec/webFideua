0b41746d903e6cdfddd32cdcc3e4a90f497c3d0d
jordi carlos <jordi.carlos@rusc.net>
2021-04-18 11:23:32 +0200

    actualització global

--- public/js/code.js
+++ public/js/code.js
@@ -31,27 +31,23 @@ simplyCountdown('#contador', {
 // Funció que obté els valors entrats a el formulari HTML, i que amb els mateixos inicialitza un objecte data que el passarem al 
 // navegador (POST) a una ruta determinada (/api) indicant que el body (cos) del document serà tipus JSON.
 function RegistreFamilia() {
-   const nomFamilia = document.getElementById("nom-familia");
-   const numMembres = document.getElementById("num-membres");
+   const nomFamilia = document.getElementById("nomFamilia");
+   const numMembres = document.getElementById("numMembres");
    
    if (nomFamilia.value === "") {
        alert("Upps!... No has omplert els camps!");   
    } else {
-       const paginaReg = window.open("","nomFamilia.value, numMembres.value","top=300,left=700,width=350,height=270,toolbar=no,resizable=no"); 
-       
-       paginaReg.document.write("<title>Inscripció Fideuà 2021 (Escola Mireia)</title><body style=background-color:white;><h3 style=font-family:Verdana;text-align:center;color:green;>Inscripció realitzada correctament!</h3><hr style=height:5px;color:black;background-color:grey;border-width:0px;><span style=font-family:Verdana;font-weight:bold;>Nom família: </span>" +
-                               nomFamilia.value + "<br><span style=font-family:Verdana;font-weight:bold;>Nombre de persones: </span>" + numMembres.value +
-                               "<br><hr style=height:15px;color:grey;background-color:grey;border-width:0px;><center><input type=button value=Dacord onclick=window.close();><h5 style=font-family:Verdana;text-align:center;color:grey;>Fideuà 2021 Escola Mireia de Barcelona</h5></center></body>"                        
-                               );
-   
+     
    // Capturo les dades del formulari HTML, les inicialitzo en un objecte tipus JSON i ho indico en el fetch.
    const NomFamilia = nomFamilia.value;
    const NumMembres = numMembres.value;
-   const data = { NomFamilia, NumMembres};
+   const data = {NomFamilia, NumMembres};
    const options = {
+      // Consultar MDN Mozzilla per veure totes les opcions.
       method: 'POST',
       headers: {
-          'Content-Type': 'application/json'
+          'Content-Type': 'application/json',
+          'Access-Control-Allow-Origin':'*'
       },
       body: JSON.stringify(data)
    };
@@ -59,30 +55,35 @@ function RegistreFamilia() {
    
    EsborraFormulari(); // Elimino les dades entrades al formulari (perque quedi net).
    location.reload(); // Refresco la pàgina HTML per reflexar el registre acabat d'entrar (i no haver d'esperar a que es recarregui la pàgina).
-   PintaDades(); // Mostro les dades per pantalla HTML.       
-   }
- }
+   };
+  PintaDades(); // Mostro les dades per pantalla HTML.        
+};
 
 function EsborraFormulari() {
-    let nomFamiliaInput = document.getElementById("nom-familia");
-    let numMembresInput = document.getElementById("num-membres");
+    let nomFamiliaInput = document.getElementById("nomFamilia");
+    let numMembresInput = document.getElementById("numMembres");
         
     let nomFamilia = "";
     let numMembres = "";
     
     nomFamiliaInput.value = nomFamilia;
     numMembresInput.value = numMembres;
-}
+};
 
 // Funció que mostra en una posició-zona de la pàgina HTML el contingut
 // de la BBDD documental (fitxer 'fideua2021').
 async function PintaDades() {
     const response = await fetch('/api'); // Busco el que hi ha a la ruta /api i ho coloco a la constant response.
     const data = await response.json(); // Creo la const data amb el contingut de response que li dic que serà del tipus objecte dades json.
-    const itemList = document.getElementById("mostra-familia"); // Creo constant que té la posició el tag HTML (identificador) on es mostrarán les dades.
-    
+    const itemList = document.getElementById("mostraFamilia"); // Creo constant que té la posició el tag HTML (identificador) on es mostrarán les dades.
+    const itemNumFamiliesInscrites = document.getElementById("numFamiliesInscrites");
+
     // Recorro el array d'objectes json i els vaig passant (DOM) per pantalla HTML.
     for (item of data) {
+       //console.log(item);
        itemList.innerHTML += item.NomFamilia + " (" + item.NumMembres + ")" + "<br>";    
-    }
-}
+    };
+
+    let numFamiliesInscrites = data.length;
+    itemNumFamiliesInscrites.innerHTML = numFamiliesInscrites;    
+};
